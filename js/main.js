7dea5c78259e3f13ee1ccaeedea03b0399810fa8
Sibo Song <siso8575@colorado.edu>
2018-02-08 20:17:05 -0700

    Completed content. Prettied. Deleted unused sources.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,11 @@
 /* 1. Scroll to top of the page */
 function Func_top() {
     $('html, body').animate({scrollTop:0}, 1500);
+    $("#c1").fadeOut(500);
+    $("#c2").fadeOut(500);
+    $("#c3").fadeOut(500);
+    $("#c0").fadeIn(500);
+    document.getElementById("myTitle").innerHTML = "welcome";
 }
 
 /* Comment out since change whole style for homepage. 
@@ -24,26 +29,26 @@ function Func_me() {
 
 /* 2. Change showing case */
 function Func_me() {
-	$("#c0").fadeOut(1000);
-    $("#c2").fadeOut(1000);
-    $("#c3").fadeOut(1000);
-    $("#c1").fadeIn(1000);
+	$("#c0").fadeOut(750);
+    $("#c2").fadeOut(750);
+    $("#c3").fadeOut(750);
+    $("#c1").fadeIn(750);
     document.getElementById("myTitle").innerHTML = "about me";
 }
 
 function Func_pur() {
-	$("#c0").fadeOut(1000);
-    $("#c1").fadeOut(1000);
-    $("#c3").fadeOut(1000);
-    $("#c2").fadeIn(1000);
+	$("#c0").fadeOut(750);
+    $("#c1").fadeOut(750);
+    $("#c3").fadeOut(750);
+    $("#c2").fadeIn(750);
     document.getElementById("myTitle").innerHTML = "my purpose";
 }
 
 function Func_con() {
-	$("#c0").fadeOut(1000);
-    $("#c1").fadeOut(1000);
-    $("#c2").fadeOut(1000);
-    $("#c3").fadeIn(1000);
+	$("#c0").fadeOut(750);
+    $("#c1").fadeOut(750);
+    $("#c2").fadeOut(750);
+    $("#c3").fadeIn(750);
     document.getElementById("myTitle").innerHTML = "contact me";
 }
 
